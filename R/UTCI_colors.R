85b2190d534b036214b02473d94166ff79b3f735
José A. Rodríguez Gallego <96689554+RodGal-2020@users.noreply.github.com>
2023-05-31 13:22:18 +0200

    Some comments added

--- R/UTCI_colors.R
+++ R/UTCI_colors.R
@@ -21,6 +21,7 @@
 #' This is a warning
 #'
 #' @export
-UTCI_colors = c("#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF", "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026")
+UTCI_colors = c("#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF", "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026") # brewer.pal(n = 11, name = 'RdYlBu')
+# library("RColorBrewer"); display.brewer.pal(n = 11, name = 'RdYlBu')
 # UTCI_colors = rev(heat.colors(11))
 # c("blue", "dodgerblue", "green", "yellow", "orange", "red", "maroon")
