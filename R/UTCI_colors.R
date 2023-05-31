189b11c3c1ac5f2116758dfc30ccff5eb07d08aa
José A. Rodríguez Gallego <96689554+RodGal-2020@users.noreply.github.com>
2023-05-31 13:16:09 +0200

    Update palette

--- R/UTCI_colors.R
+++ R/UTCI_colors.R
@@ -21,4 +21,6 @@
 #' This is a warning
 #'
 #' @export
-UTCI_colors = heat.colors(11)
+UTCI_colors = c("#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF", "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026")
+# UTCI_colors = rev(heat.colors(11))
+# c("blue", "dodgerblue", "green", "yellow", "orange", "red", "maroon")
