e7b593b5b5797456b021b5e816e1484394d3e694
José A. Rodríguez Gallego <96689554+RodGal-2020@users.noreply.github.com>
2023-06-01 14:02:58 +0200

    Experimental steps commented

--- R/multi_outliers.R
+++ R/multi_outliers.R
@@ -82,97 +82,97 @@ multi_outliers = function(data, plot = TRUE) {
   }
 
   ### k-neighboors
-  # library(dbscan)
-
-  # Calcular la matriz de distancias
-  dist_matrix <- dist(data)
-
-  # Definir el número de vecinos más cercanos (k)
-  k <- 5
-
-  # Calcular la distancia promedio a los k vecinos más cercanos para cada punto
-  tic()
-  avg_dist <- apply(as.matrix(dist_matrix), 1, function(row) {
-    sorted_indices <- order(row)
-    mean(row[sorted_indices[2:(k + 1)]]) # Excluye la distancia al propio punto (valor mínimo)
-  })
-  toc()
-
-  # 1 min 30 s para 11 elementos.
-
-  # Calcular los límites para identificar outliers
-  q1 <- quantile(avg_dist, 0.25)
-  q3 <- quantile(avg_dist, 0.75)
-  iqr <- q3 - q1
-  lower_limit <- q1 - 1.5 * iqr
-  upper_limit <- q3 + 1.5 * iqr
-
-  # Identificar outliers
-  outliers <- which(avg_dist < lower_limit | avg_dist > upper_limit)
-
-  # Imprimir los índices de los outliers identificados
-  print(outliers)
-
-  ### Z-score
-  # Calcular el z-score para cada variable
-  z_scores <- apply(data, 2, function(x) (x - mean(x)) / sd(x))
-
-  # Definir umbrales basados en el z-score
-  z_score_threshold <- 3
-
-  # Identificar outliers utilizando el z-score
-  outliers_z_score <- apply(z_scores, 2, function(x) abs(x) > z_score_threshold)
-
-  # Calcular los límites del rango intercuartílico (IQR) para cada variable
-  q1 <- apply(data, 2, quantile, probs = 0.25)
-  q3 <- apply(data, 2, quantile, probs = 0.75)
-  iqr <- q3 - q1
-
-  # Definir umbrales basados en el rango intercuartílico (IQR)
-  iqr_threshold <- 1.5
-
-  # Identificar outliers utilizando el rango intercuartílico (IQR)
-  outliers_iqr <- apply(data, 2, function(x) x < (q1 - iqr_threshold * iqr) | x > (q3 + iqr_threshold * iqr))
-
-  # Combinar los resultados de ambos métodos
-  outliers <- outliers_z_score | outliers_iqr
-
-  # Imprimir los índices de los outliers identificados para cada variable
-  for (i in 1:ncol(data)) {
-    print(paste("Variable:", names(data)[i]))
-    print(which(outliers[, i]))
-  }
-
-  ## z-scores
-  # Calcular el z-score para cada variable
-  z_scores <- apply(data, 2, function(x) (x - mean(x)) / sd(x))
-
-  # Definir umbrales basados en el z-score
-  z_score_threshold <- 3
-
-  # Identificar outliers utilizando el z-score
-  outliers_z_score <- apply(z_scores, 2, function(x) abs(x) > z_score_threshold)
-
-  # Calcular los límites del rango intercuartílico (IQR) para cada variable
-  q1 <- apply(data, 2, quantile, probs = 0.25)
-  q3 <- apply(data, 2, quantile, probs = 0.75)
-  iqr <- q3 - q1
-
-  # Definir umbrales basados en el rango intercuartílico (IQR)
-  iqr_threshold <- 1.5
-
-  # Identificar outliers utilizando el rango intercuartílico (IQR)
-  outliers_iqr <- apply(data, 2, function(x) x < (q1 - iqr_threshold * iqr) | x > (q3 + iqr_threshold * iqr))
-
-  # Combinar los resultados de ambos métodos
-  outliers <- outliers_z_score | outliers_iqr
-
-  # Imprimir los índices de los outliers identificados para cada variable
-  ## CUIDADO! Que todos sean outliers suena como mínimo sospechoso
-  for (i in 1:ncol(data)) {
-    print(paste("Variable:", names(data)[i]))
-    print(which(outliers[, i]))
-  }
+  # # library(dbscan)
+  #
+  # # Calcular la matriz de distancias
+  # dist_matrix <- dist(data)
+  #
+  # # Definir el número de vecinos más cercanos (k)
+  # k <- 5
+  #
+  # # Calcular la distancia promedio a los k vecinos más cercanos para cada punto
+  # tic()
+  # avg_dist <- apply(as.matrix(dist_matrix), 1, function(row) {
+  #   sorted_indices <- order(row)
+  #   mean(row[sorted_indices[2:(k + 1)]]) # Excluye la distancia al propio punto (valor mínimo)
+  # })
+  # toc()
+  #
+  # # 1 min 30 s para 11 elementos.
+  #
+  # # Calcular los límites para identificar outliers
+  # q1 <- quantile(avg_dist, 0.25)
+  # q3 <- quantile(avg_dist, 0.75)
+  # iqr <- q3 - q1
+  # lower_limit <- q1 - 1.5 * iqr
+  # upper_limit <- q3 + 1.5 * iqr
+  #
+  # # Identificar outliers
+  # outliers <- which(avg_dist < lower_limit | avg_dist > upper_limit)
+  #
+  # # Imprimir los índices de los outliers identificados
+  # print(outliers)
+  #
+  # ### Z-score
+  # # Calcular el z-score para cada variable
+  # z_scores <- apply(data, 2, function(x) (x - mean(x)) / sd(x))
+  #
+  # # Definir umbrales basados en el z-score
+  # z_score_threshold <- 3
+  #
+  # # Identificar outliers utilizando el z-score
+  # outliers_z_score <- apply(z_scores, 2, function(x) abs(x) > z_score_threshold)
+  #
+  # # Calcular los límites del rango intercuartílico (IQR) para cada variable
+  # q1 <- apply(data, 2, quantile, probs = 0.25)
+  # q3 <- apply(data, 2, quantile, probs = 0.75)
+  # iqr <- q3 - q1
+  #
+  # # Definir umbrales basados en el rango intercuartílico (IQR)
+  # iqr_threshold <- 1.5
+  #
+  # # Identificar outliers utilizando el rango intercuartílico (IQR)
+  # outliers_iqr <- apply(data, 2, function(x) x < (q1 - iqr_threshold * iqr) | x > (q3 + iqr_threshold * iqr))
+  #
+  # # Combinar los resultados de ambos métodos
+  # outliers <- outliers_z_score | outliers_iqr
+  #
+  # # Imprimir los índices de los outliers identificados para cada variable
+  # for (i in 1:ncol(data)) {
+  #   print(paste("Variable:", names(data)[i]))
+  #   print(which(outliers[, i]))
+  # }
+  #
+  # ## z-scores
+  # # Calcular el z-score para cada variable
+  # z_scores <- apply(data, 2, function(x) (x - mean(x)) / sd(x))
+  #
+  # # Definir umbrales basados en el z-score
+  # z_score_threshold <- 3
+  #
+  # # Identificar outliers utilizando el z-score
+  # outliers_z_score <- apply(z_scores, 2, function(x) abs(x) > z_score_threshold)
+  #
+  # # Calcular los límites del rango intercuartílico (IQR) para cada variable
+  # q1 <- apply(data, 2, quantile, probs = 0.25)
+  # q3 <- apply(data, 2, quantile, probs = 0.75)
+  # iqr <- q3 - q1
+  #
+  # # Definir umbrales basados en el rango intercuartílico (IQR)
+  # iqr_threshold <- 1.5
+  #
+  # # Identificar outliers utilizando el rango intercuartílico (IQR)
+  # outliers_iqr <- apply(data, 2, function(x) x < (q1 - iqr_threshold * iqr) | x > (q3 + iqr_threshold * iqr))
+  #
+  # # Combinar los resultados de ambos métodos
+  # outliers <- outliers_z_score | outliers_iqr
+  #
+  # # Imprimir los índices de los outliers identificados para cada variable
+  # ## CUIDADO! Que todos sean outliers suena como mínimo sospechoso
+  # for (i in 1:ncol(data)) {
+  #   print(paste("Variable:", names(data)[i]))
+  #   print(which(outliers[, i]))
+  # }
 
   return(output)
 }
